ffae6ca2cec9e86368983c5b7bb648d509d75edb
danuhanu <danuhanu@gmail.com>
2016-04-07 20:20:20 -0700

    magic 8 ball and looper updates

---
+++ 06-Week/looper/js/app.js
@@ -0,0 +1,22 @@
+var responses = ["yes", "no", 'ask again later', 'maybe', 'reply hazy', 'signs point to yes']
+
+// This waits till the page finishes loading before running the code inside the {}
+$(document).ready(function() {
+
+  console.log(responses)
+
+  responses.forEach(
+    function(response){
+      // alert("Take one down, pass it around...")
+      // alert("you took down the " + response)
+      document.write(response + "<br>")
+
+    }
+  )
+
+  // document.write("You have " + responses.length + " items")
+  document.write("<hr>")
+document.write("You have " + responses.length + " Items")
+
+
+});
---
+++ Magic 8 Ball/js/app.js
@@ -0,0 +1,13 @@
+// This waits till the page finishes loading before running the code inside the {}
+$(document).ready(function() {
+  var responses = ["yes", "no", "ask again", "later", "maybe", "reply hazy", "signs point to yes"]
+
+  function buttonClicked(){
+    var  = $('#').val()
+    var randomNumber = Math.floor(Math.random() * prefix.length)
+    $('#output').text()
+  }
+
+  $('#submit').click(buttonClicked)
+
+});
