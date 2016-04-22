9ee12380caffe44abf393007fd909b2eaceb7fef
danuhanu <danuhanu@gmail.com>
2016-04-21 19:06:47 -0700

    Week 8 updates and Final Project updates

--- My Final Project/app.js
+++ My Final Project/app.js
@@ -1,21 +1,6 @@
-
-// $(function() {
-  // var headerHeight = $('header').height();
-  // $('a[href*="#"]:not([href="#"])').click(function() {
-    // if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      // var target = $(this.hash);
-      // target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      // if (target.length) {
-        // $('html, body').animate({
-          // scrollTop: target.offset().top - headerHeight
-        // }, 1000);
-        // return false;
-      // }
-    // }
-  // });
-// });
-
 console.clear();
+
+$(function() {
 
 $('nav li a').click(function(){
   var headerHeight = $('header').height();
@@ -32,5 +17,5 @@ $('nav li a').click(function(){
   }, 1000);
 
 
-
-})
+  });
+});
